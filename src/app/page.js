94ac44f610c53610594111e4fb6ded8ba5690337
nikitashirikov <shirikofnikita@gmail.com>
2023-08-27 12:25:37 +0300

    First

--- src/app/page.js
+++ src/app/page.js
@@ -4,92 +4,42 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+      <section className={styles.lightSection}>
+        <p className={styles.lightText}>
+          My name is Shirikov Nikita
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+        <p className={styles.attentionText}>
+          Hi
+        </p>
+        <p className={styles.normalLightText}>
+          and this is my website, feel cozy
+        </p>
+      </section>
+      <section className={styles.bioSection}>
+        <p className={styles.itemAboutMe}>
+          I am web-developer and CS HSE student
+        </p>
+        <p className={styles.itemAboutMe2}>
+          I belive that studying is practice
+        </p>
+        <a href="https://github.com/PlatinaEnjoyer" className={styles.ga}>
+          so all info about me you can find here
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section className={styles.contactSection}>
+        <p className={styles.contactText1}>
+          Contacts
+        </p>
+        <p className={styles.contactText2}>
+          telegram: @SNick5
+        </p>
+        <p className={styles.contactText3}>
+          github: PlatinaEnjoyer
+        </p>
+        <p className={styles.contactText3}>
+          mail: shirikovn@mail.ru
+        </p>
+      </section>
     </main>
   )
 }
